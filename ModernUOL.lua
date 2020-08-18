7f25136eb5f43c736b41024139f6482a7361b546
Guillaume <guillaumebrulet1995@gmail.colm>
2020-08-18 10:04:03 +0200

    Added Legit Orbwalker

--- ModernUOL.lua
+++ ModernUOL.lua
@@ -119,120 +119,342 @@ local ModernUOL = class(ModernUOLAbstract)
 
 function ModernUOL:init()
     self.super.init(self)
-
-    self.OrbState = {}
 end
 
-function ModernUOL:GetTarget(range, damageType, from)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        return _G.LegitOrbwalker:GetTarget(range, damageType, from)
-    end
-end
-
-function ModernUOL:GetMode()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        return _G.LegitOrbwalker:GetMode()
-    end
-end
-
-function ModernUOL:ForceTarget(unit)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:SetForcedTarget(unit)
-    end
-
-    self.OrbState.ForcedTarget = unit
-end
-
-function ModernUOL:ResetForcedTarget()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:UnsetForcedTarget()
-    end
-
-    self.OrbState.ForcedTarget = nil
-end
-
-function ModernUOL:GetForcedTarget()
-    return self.OrbState.ForcedTarget
-end
-
+--[[
+    function: ModernUOL:IsAttacking()
+    Parameter: None
+    Return: Boolean
+    Comment: Return true if my hero is attacking
+--]]
 function ModernUOL:IsAttacking()
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
         return _G.LegitOrbwalker:IsAttacking()
     end
 end
 
+--[[
+    function: ModernUOL:IsOrbWalking()
+    Parameter: None
+    Return: Boolean
+    Comment: Return true if my hero is orbwalking
+--]]
+function ModernUOL:IsOrbWalking()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:IsOrbWalking()
+    end
+end
+
+--[[
+    function: ModernUOL:CanAttack()
+    Parameter: None
+    Return: Boolean
+    Comment: Return true if my hero can auto attack
+--]]
 function ModernUOL:CanAttack()
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
         return _G.LegitOrbwalker:CanAttack()
     end
 end
 
+--[[
+    function: ModernUOL:CanMove()
+    Parameter: None
+    Return: Boolean
+    Comment: Return true if my hero can move without cancel auto attack
+--]]
 function ModernUOL:CanMove()
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
         return _G.LegitOrbwalker:CanMove()
     end
 end
 
-function ModernUOL:GetDamagePrediction(object, delay)
+--[[
+    function: ModernUOL:MoveTo()
+    Parameter:
+        1. D3DXVECTOR3
+    Return: nil
+    Comment: Issue order a move to the given position if orbwalker can move
+--]]
+function ModernUOL:MoveTo(position)
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        return object.health - _G.LegitOrbwalker:HpPred(object, delay)
+        return _G.LegitOrbwalker:MoveTo(position)
     end
 end
 
-function ModernUOL:GetHealthPrediction(object, delay)
+--[[
+    function: ModernUOL:Attack()
+    Parameter:
+        1. gameObject
+    Return: nil
+    Comment: Issue order a attack to the given object (No range/unit check) if orbwalker can attack
+--]]
+function ModernUOL:Attack(object)
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        return _G.LegitOrbwalker:HpPred(object, delay)
+        return _G.LegitOrbwalker:Attack(object)
     end
 end
 
-function ModernUOL:OrbwalkTo(position)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:MoveTo(position)
-    end
-
-    self.OrbState.OrbwalkTo = position
-end
-
-function ModernUOL:ResetOrbwalkTo()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:MoveTo()
-    end
-
-    self.OrbState.OrbwalkTo = nil
-end
-
-function ModernUOL:IsOrbwalkToSet()
-    return self.OrbState.OrbwalkTo
-end
-
-function ModernUOL:Attack(unit)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:Attack(unit)
-    end
-end
-
+--[[
+    function: ModernUOL:BlockAttack()
+    Parameter:
+        1. boolean
+    Return: nil
+    Comment: Block orbwalker attack
+--]]
 function ModernUOL:BlockAttack(bool)
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:BlockAttack(bool)
+        return _G.LegitOrbwalker:BlockAttack(bool)
     end
-
-    self.OrbState.BlockAttack = bool
 end
 
-function ModernUOL:IsBlockedAttack()
-    return self.OrbState.BlockAttack
-end
-
+--[[
+    function: ModernUOL:BlockMove()
+    Parameter:
+        1. boolean
+    Return: nil
+    Comment: Block Orbwalker movement
+--]]
 function ModernUOL:BlockMove(bool)
     if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
-        _G.LegitOrbwalker:BlockMove(bool)
+        return _G.LegitOrbwalker:BlockMove(bool)
     end
-
-    self.OrbState.BlockMove = bool
 end
 
-function ModernUOL:IsBlockedMove()
-    return self.OrbState.BlockMove
+--[[
+    function: ModernUOL:IsAttackBlocked()
+    Parameter: None
+    Return: boolean
+    Comment: Return if orbwalker attack is currently blocked
+--]]
+function ModernUOL:IsAttackBlocked()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:IsAttackBlocked()
+    end
+end
+
+--[[
+    function: ModernUOL:IsMoveBlocked()
+    Parameter: None
+    Return: boolean
+    Comment: Return if orbwalker movement is currently blocked
+--]]
+function ModernUOL:IsMoveBlocked()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:IsMoveBlocked()
+    end
+end
+
+--[[
+    function: ModernUOL:GetMode()
+    Parameter: None
+    Return: string ("Combo", "Waveclear", "Harass", "Lasthit", "none")
+    Comment: Return active orbwalker mode
+--]]
+function ModernUOL:GetMode()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetMode()
+    end
+end
+
+--[[
+    function: ModernUOL:ResetAA()
+    Parameter: None
+    Return: nil
+    Comment: Reset orbwalker auto attack cooldown
+--]]
+function ModernUOL:ResetAA()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:ResetAA()
+    end
+end
+
+--[[
+    function: ModernUOL:GetTarget(range, position)
+    Parameter: int, D3DXVECTOR3
+    Return: gameObject or nil
+    Comment: Return current orbwalker target (minions/structures included)
+--]]
+function ModernUOL:GetTarget(range, position)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetTarget(range, position)
+    end
+end
+
+--[[
+    function: ModernUOL:GetHeroTarget(range, position)
+    Parameter: int, D3DXVECTOR3
+    Return: gameObject or nil
+    Comment: Returns hero target
+--]]
+function ModernUOL:GetHeroTarget(range, position)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetHeroTarget(range, position)
+    end
+end
+
+--[[
+    function: ModernUOL:SetTarget(target, time)
+    Parameter: gameObject, time (optional and in second)
+    Return: nil
+    Comment: Force Orbwalker to target the given gameOject during time (time is optional)
+--]]
+function ModernUOL:SetTarget(target, time)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:SetTarget(target, time)
+    end
+end
+
+--[[
+    function: ModernUOL:UnSetTarget()
+    Parameter: None
+    Return: nil
+    Comment: Unset the forced target
+--]]
+function ModernUOL:UnSetTarget()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:UnSetTarget()
+    end
+end
+
+--[[
+    function: ModernUOL:GetForcedTarget()
+    Parameter: None
+    Return: gameObject or nil
+    Comment: Get forced target object
+--]]
+function ModernUOL:GetForcedTarget()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetForcedTarget()
+    end
+end
+
+--[[
+    function: ModernUOL:WaitingForMinion()
+    Parameter: None
+    Return: boolean
+    Comment: Return true if orbwalker is waiting for a minion to last hit
+--]]
+function ModernUOL:WaitingForMinion()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:WaitingForMinion()
+    end
+end
+
+--[[
+    function: ModernUOL:GetMinions()
+    Parameter: None
+    Return: table
+    Comment: Return only minions present on lane (mage, cannon, melee, super)
+--]]
+function ModernUOL:GetMinions()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetMinions()
+    end
+end
+
+--[[
+    function: ModernUOL:HpPred(unit, time)
+    Parameter: gameObject, time (sec/ms ?)
+    Return: float
+    Comment: Return predicted unit health after the given time
+--]]
+function ModernUOL:HpPred(unit, time)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:HpPred(unit, time)
+    end
+end
+
+--[[
+    function: ModernUOL:AutoAttackCooldown()
+    Parameter: None
+    Return: float
+    Comment: Return the next auto attack time
+--]]
+function ModernUOL:AutoAttackCooldown()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:AutoAttackCooldown()
+    end
+end
+
+--[[
+    function: ModernUOL:AutoAttackOnCooldown()
+    Parameter: None
+    Return: boolean
+    Comment: Return if auto attack is on cooldown
+--]]
+function ModernUOL:AutoAttackOnCooldown()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:AutoAttackOnCooldown()
+    end
+end
+
+--[[
+    function: ModernUOL:AttackSpeed()
+    Parameter: None
+    Return: float
+    Comment: Return the actual attack speed of myHero
+--]]
+function ModernUOL:AttackSpeed()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:AttackSpeed()
+    end
+end
+
+--[[
+    function: ModernUOL:GetProjectileSpeed(gameObject)
+    Parameter: gameObject
+    Return: int
+    Comment: Return projectile speed of given gameObject
+--]]
+function ModernUOL:GetProjectileSpeed(gameObject)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetProjectileSpeed(gameObject)
+    end
+end
+
+--[[
+    function: ModernUOL:AddCallback(callback, func)
+    Parameter: string, function
+        callback's:
+            OnUnKillable -- Trigger if it's impossible to last hit | arg: GameObject//minion
+            OnAttack -- Triggers after an AA is recognized
+            OnAfterAttack -- Triggers after an AA is done
+            OnBeforeAttack -- Triggers just before an AA is recognized
+            OnBeforeMovement -- Triggers just before a movement is recognized | arg: Position
+            CanMove -- Triggers when the Champion can move again
+            CanAttack -- Triggers when the Champion can AA again
+    Return: nil
+    Comment: Register a callback
+--]]
+function ModernUOL:AddCallback(callback, func)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:AddCallback(callback, func)
+    end
+end
+
+--[[
+    function: ModernUOL:RemoveCallback(func)
+    Parameter: function
+    Return: nil
+    Comment: Remove a function from callback
+--]]
+function ModernUOL:RemoveCallback(func)
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:RemoveCallback(func)
+    end
+end
+
+--[[
+    function: ModernUOL:GetCallbacks()
+    Parameter: function
+    Return: table
+    Comment: Get table of registered callback
+--]]
+function ModernUOL:GetCallbacks()
+    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+        return _G.LegitOrbwalker:GetCallbacks()
+    end
 end
 
 --////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
