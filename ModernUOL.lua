b3e6458f6983dc5ff42bd5c35e976d324bd94666
Feez <dontmesswithfeez@gmail.com>
2020-10-07 03:12:05 -0500

    Add wadbot

--- ModernUOL.lua
+++ ModernUOL.lua
@@ -1,7 +1,7 @@
 --////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 --////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-local version = 1.2
+local version = 1.3
 GetInternalWebResultAsync('ModernUOL.version', function(v)
     if v and tonumber(v) > version then
         DownloadInternalFileAsync('ModernUOL.lua', _G.DEFAULT_COMMON_PATH, function(success)
@@ -31,6 +31,8 @@ end
 
 local ModernUOLAbstract = class()
 
+local FakeScript = {WADBOT = true}
+
 ModernUOLAbstract.SupportedOrbwalkers = {
     [_G.PaidScript.MED] =
         {
@@ -52,7 +54,7 @@ ModernUOLAbstract.SupportedOrbwalkers = {
             Name = "Legit Orbwalker: Reborn",
             Valid =
                 function()
-                    return _G.LegitOrbwalker
+                    return _G.LegitOrbwalker and not _G.LegitOrbwalker.fakeIndex
                 end
         },
     [_G.PaidScript.RMAN_LOADER] =
@@ -60,10 +62,20 @@ ModernUOLAbstract.SupportedOrbwalkers = {
             Name = "RMAN Orbwalker",
             Valid =
                 function()
-                    return _G.LegitOrbwalker -- RMAN supports same exact API
+                    return _G.LegitOrbwalker and not _G.LegitOrbwalker.fakeIndex
+                end
+        },
+    [FakeScript.WADBOT] =
+        {
+            Name = "Wadbot",
+            Valid =
+                function()
+                    return _G.LegitOrbwalker and _G.LegitOrbwalker.fakeIndex
                 end
         },
 }
+
+local IS_LEGIT_ORB_COMPATIBLE = {[_G.PaidScript.REBORN_ORB] = true, [_G.PaidScript.RMAN_LOADER] = true, [FakeScript.WADBOT] = true}
 
 function ModernUOLAbstract:init()
     -- self.requireAuroraOrbAPI = false
@@ -214,8 +226,9 @@ end
     Return: Boolean
     Comment: Return true if my hero is attacking
 --]]
+
 function ModernUOL:IsAttacking()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:IsAttacking()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:IsAttacking()
@@ -231,7 +244,7 @@ end
     Comment: Return true if my hero is orbwalking
 --]]
 function ModernUOL:IsOrbWalking()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:IsOrbWalking()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:IsOrbwalking()
@@ -247,7 +260,7 @@ end
     Comment: Return true if my hero can auto attack
 --]]
 function ModernUOL:CanAttack()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:CanAttack()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:CanAttack()
@@ -263,7 +276,7 @@ end
     Comment: Return true if my hero can move without cancel auto attack
 --]]
 function ModernUOL:CanMove()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:CanMove()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:CanMove()
@@ -280,7 +293,7 @@ end
     Comment: Issue order a move to the given position if orbwalker can move
 --]]
 function ModernUOL:MoveTo(position)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:MoveTo(position)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:Move(position)
@@ -297,7 +310,7 @@ end
     Comment: Issue order a attack to the given object (No range/unit check) if orbwalker can attack
 --]]
 function ModernUOL:Attack(object)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:Attack(object)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:Attack(object)
@@ -314,7 +327,7 @@ end
     Comment: Block/Unblock orbwalker attack
 --]]
 function ModernUOL:BlockAttack(bool)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:BlockAttack(bool)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:BlockAttack(bool)
@@ -331,7 +344,7 @@ end
     Comment: Block/Unblock Orbwalker movement
 --]]
 function ModernUOL:BlockMove(bool)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:BlockMove(bool)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:BlockMove(bool)
@@ -347,7 +360,7 @@ end
     Comment: Return if orbwalker attack is currently blocked
 --]]
 function ModernUOL:IsAttackBlocked()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:IsAttackBlocked()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:IsAttackBlocked()
@@ -363,7 +376,7 @@ end
     Comment: Return if orbwalker movement is currently blocked
 --]]
 function ModernUOL:IsMoveBlocked()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:IsMoveBlocked()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:IsMoveBlocked()
@@ -379,7 +392,7 @@ end
     Comment: Return active orbwalker mode
 --]]
 function ModernUOL:GetMode()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetMode()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         if _G.AuroraOrb.Orbwalker:IsComboKeyPress() then return "Combo"
@@ -402,7 +415,7 @@ end
     Comment: Reset orbwalker auto attack cooldown
 --]]
 function ModernUOL:ResetAA()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:ResetAA()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:ResetAttack()
@@ -418,7 +431,7 @@ end
     Comment: Return hero target
 --]]
 function ModernUOL:GetTarget(range, position)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetHeroTarget(range, position)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetTargetSelectorTarget(range, position)
@@ -434,7 +447,7 @@ end
     Comment: Returns the current orbwalker target (can be all unit)
 --]]
 function ModernUOL:GetCurrentTarget()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetTarget()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetCurrentTarget()
@@ -450,7 +463,7 @@ end
     Comment: Force Orbwalker to target the given gameOject during time (time is optional)
 --]]
 function ModernUOL:SetTarget(target, time)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:SetTarget(target, time)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:SetTarget(target, time)
@@ -466,7 +479,7 @@ end
     Comment: Unset the forced target
 --]]
 function ModernUOL:UnSetTarget()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:UnSetTarget()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:UnSetTarget()
@@ -482,7 +495,7 @@ end
     Comment: Get forced target object
 --]]
 function ModernUOL:GetForcedTarget()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetForcedTarget()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetForcedTarget()
@@ -498,7 +511,7 @@ end
     Comment: Return true if orbwalker is waiting for a minion to last hit
 --]]
 function ModernUOL:WaitingForMinion()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:WaitingForMinion()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:WaitingForMinion()
@@ -514,7 +527,7 @@ end
     Comment: Return only minions present on lane (mage, cannon, melee, super)
 --]]
 function ModernUOL:GetMinions()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetMinions()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Minion:GetEnemy(function(a) return _G.AuroraOrb.Minion:IsLaneMinion(a) end)
@@ -530,7 +543,7 @@ end
     Comment: Return predicted unit health after the given time
 --]]
 function ModernUOL:HpPred(unit, time)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:HpPred(unit, time)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Health:Get(unit, time * 1000)
@@ -546,7 +559,7 @@ end
     Comment: Return the next auto attack time in second
 --]]
 function ModernUOL:AutoAttackCooldown()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:AutoAttackCooldown()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetAutoAttackCooldownRemaining()
@@ -562,7 +575,7 @@ end
     Comment: Return if auto attack is on cooldown
 --]]
 function ModernUOL:AutoAttackOnCooldown()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:AutoAttackOnCooldown()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:AutoAttackOnCooldown()
@@ -578,7 +591,7 @@ end
     Comment: Return the actual attack speed of myHero
 --]]
 function ModernUOL:AttackSpeed()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:AttackSpeed()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.MyHero:GetAttackSpeed()
@@ -595,7 +608,7 @@ end
     Comment: Return projectile speed of given gameObject
 --]]
 function ModernUOL:GetProjectileSpeed(gameObject)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetProjectileSpeed(gameObject)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetProjectileSpeed(gameObject)
@@ -620,7 +633,7 @@ end
     Comment: Register a callback
 --]]
 function ModernUOL:AddCallback(callback, func)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:AddCallback(callback, func)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         if callback == "OnUnKillable" then return _G.AuroraOrb.Orbwalker:RegisterOnMinionNonLastHitableFunction(func)
@@ -643,7 +656,7 @@ end
     Comment: Remove a function from callback
 --]]
 function ModernUOL:RemoveCallback(func)
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:RemoveCallback(func)
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:RemoveCallback(func)
@@ -657,7 +670,7 @@ end
     Comment: Get table of registered callback
 --]]
 function ModernUOL:GetCallbacks()
-    if self.ActiveOrb == _G.PaidScript.REBORN_ORB then
+    if IS_LEGIT_ORB_COMPATIBLE[self.ActiveOrb] then
         return _G.LegitOrbwalker:GetCallbacks()
     elseif self.ActiveOrb == _G.PaidScript.AURORA_ORB then
         return _G.AuroraOrb.Orbwalker:GetCallbacks()
