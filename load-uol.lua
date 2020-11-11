f1304a4a23609ae4409cc0f8afa094f183ca9efb
Feez <dontmesswithfeez@gmail.com>
2020-11-11 01:09:39 -0600

    Cleanup

--- load-uol.lua
+++
@@ -1,8 +0,0 @@
---- For overriding Common version in custom scripts directory
-local prev = package.path
-
-package.path = _G.SCRIPT_PATH .. "?.lua;" .. prev
-
-require("ModernUOL")
-
-package.path = prev
